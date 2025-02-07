8fb26c73ed409f4d6c5d06ba5d7acfa586f801b3
Larysa <draglara@ukr.net>
2025-02-07 20:01:55 +0200

    section projects

---
+++ src/js/projects.js
@@ -0,0 +1,28 @@
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+
+// const swiper = new Swiper('.swiper', {
+//   slidesPerView: 1,
+//   // spaceBetween: 20,
+//   // loop: false,
+//   // effect: 'slider',
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//   },
+//   mousewheel: {
+//     forceToAxis: true,
+//   },
+//   grabCursor: true,
+//   simulateTouch: true,
+//   touchRatio: 1,
+// });
+
+// const swiperContainer = document.querySelector('.swiper').swiper;
+
+// // Now you can use all slider methods like
+// swiper.slideNext();
