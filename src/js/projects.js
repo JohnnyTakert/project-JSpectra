0c73cd87fb1f0dcb7b88b907515ef72755a091d2
Larysa <draglara@ukr.net>
2025-02-08 18:02:14 +0200

    add section Projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,28 +1,52 @@
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-// const swiper = new Swiper('.swiper', {
-//   slidesPerView: 1,
-//   // spaceBetween: 20,
-//   // loop: false,
-//   // effect: 'slider',
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//   },
-//   mousewheel: {
-//     forceToAxis: true,
-//   },
-//   grabCursor: true,
-//   simulateTouch: true,
-//   touchRatio: 1,
-// });
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  allowTouchMove: true,
+  loop: false,
+  on: {
+    slideChange: function () {
+      updateButtons(this);
+    },
+  },
+});
 
-// const swiperContainer = document.querySelector('.swiper').swiper;
+const prevButton = document.querySelector('.btn-prev');
+const nextButton = document.querySelector('.btn-next');
 
-// // Now you can use all slider methods like
-// swiper.slideNext();
+nextButton.addEventListener('click', function () {
+  swiper.slideNext();
+});
+
+prevButton.addEventListener('click', function () {
+  swiper.slidePrev();
+});
+
+// Перевірка на початок і кінець слайдера
+function updateButtons(swiper) {
+  if (swiper.isBeginning) {
+    prevButton.classList.add('disabled');
+    prevButton.setAttribute('disabled', 'true');
+  } else {
+    prevButton.classList.remove('disabled');
+    prevButton.removeAttribute('disabled');
+  }
+
+  if (swiper.isEnd) {
+    nextButton.classList.add('disabled');
+    nextButton.setAttribute('disabled', 'true');
+  } else {
+    nextButton.classList.remove('disabled');
+    nextButton.removeAttribute('disabled');
+  }
+}
