b1966571d7c4c79774079a93677f9ddb608f6b3a
Larysa <draglara@ukr.net>
2025-02-09 15:12:52 +0200

    fix projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,7 +1,9 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import { Navigation } from 'swiper/modules';
 
 const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
   slidesPerView: 1,
   spaceBetween: 0,
   navigation: {
@@ -15,40 +17,36 @@ const swiper = new Swiper('.swiper', {
   allowTouchMove: true,
   loop: false,
   on: {
+    init: function () {
+      updateButtons(this);
+    },
     slideChange: function () {
       updateButtons(this);
     },
   },
 });
 
-const prevButton = document.querySelector('.swiper-button-prev');
-const nextButton = document.querySelector('.swiper-button-next');
+// nextButton.addEventListener('click', function () {
+//   swiper.slideNext();
+// });
 
-nextButton.addEventListener('click', function () {
-  swiper.slideNext();
-});
-
-prevButton.addEventListener('click', function () {
-  swiper.slidePrev();
-});
+// prevButton.addEventListener('click', function () {
+//   swiper.slidePrev();
+// });
 
 // Перевірка на початок і кінець слайдера
 function updateButtons(swiper) {
+  const prevButton = document.querySelector('.swiper-button-prev');
+  const nextButton = document.querySelector('.swiper-button-next');
   if (swiper.isBeginning) {
-    prevButton.classList.add('disabled');
-    prevButton.setAttribute('disabled', 'true');
+    prevButton.classList.add('swiper-button-disabled');
   } else {
-    prevButton.classList.remove('disabled');
-    prevButton.removeAttribute('disabled');
+    prevButton.classList.remove('swiper-button-disabled');
   }
 
   if (swiper.isEnd) {
-    nextButton.classList.add('disabled');
-    nextButton.setAttribute('disabled', 'true');
+    nextButton.classList.add('swiper-button-disabled');
   } else {
-    nextButton.classList.remove('disabled');
-    nextButton.removeAttribute('disabled');
+    nextButton.classList.remove('swiper-button-disabled');
   }
 }
-
-updateButtons(swiper);
