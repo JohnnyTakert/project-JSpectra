32bd3d7db6785cd390d74f190a6afcd0d804cd8c
Larysa <draglara@ukr.net>
2025-02-11 17:44:29 +0200

    fixed

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,13 +2,13 @@ import Swiper from 'swiper';
 import 'swiper/css';
 import { Navigation } from 'swiper/modules';
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.projects-section .swiper', {
   modules: [Navigation],
   slidesPerView: 1,
   spaceBetween: 0,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.projects-swiper-button-next',
+    prevEl: '.projects-swiper-button-prev',
   },
   keyboard: {
     enabled: true,
@@ -29,17 +29,21 @@ const swiper = new Swiper('.swiper', {
 // Перевірка на початок і кінець слайдера
 
 function updateButtons(swiper) {
-  const prevButton = document.querySelector('.swiper-button-prev');
-  const nextButton = document.querySelector('.swiper-button-next');
+  const prevButton = document.querySelector('.projects-swiper-button-prev');
+  const nextButton = document.querySelector('.projects-swiper-button-next');
   if (swiper.isBeginning) {
+    prevButton.classList.add('disabled');
     prevButton.setAttribute('disabled', true);
   } else {
+    prevButton.classList.remove('disabled');
     prevButton.removeAttribute('disabled');
   }
 
   if (swiper.isEnd) {
+    nextButton.classList.add('disabled');
     nextButton.setAttribute('disabled', true);
   } else {
+    nextButton.classList.remove('disabled');
     nextButton.removeAttribute('disabled');
   }
 }
